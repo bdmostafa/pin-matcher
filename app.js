12face06b90849ff0257fd320dc4d3eb3f576604
Mostafa <mdmostafaalmahmud@gmail.com>
2020-08-02 22:08:03 +0600

    clear all on display input functionality

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-// Variables
+// Variables =========================================================
 let digitCount = 0;
+const min = 1000;
+const max = 9999;
 
-// Selectors
+// Selectors ===========================================================
 const displayPin = document.querySelector('.display-pin');
 const generateBtn = document.querySelector('.generate-btn');
 const submitBtn = document.querySelector('.submit-btn');
@@ -10,52 +12,55 @@ const displayDigit = document.querySelector('.display-digit');
 
 
 
-// Event Listeners
+// Event Listeners ======================================================
 document.addEventListener('DOMContentLoaded', Disabled);
 generateBtn.addEventListener('click', generatePin);
+digitBtn.forEach((digit) => digit.addEventListener('click', addDigit));
 submitBtn.addEventListener('click', submitPin);
-digitBtn.forEach((digit) => {
 
-    digit.addEventListener("click", addDigit);
+// Functions ===========================================================
 
-    // digit.style.pointerEvent = 'none';
-    // console.log(digit.target.innerText)
-})
-
-// Functions ==================================
+// When loading, SUBMIT button disabled until generate PIN
 function Disabled() {
     submitBtn.disabled = true;
     submitBtn.classList.add('disabled');
-
-    // digitBtn.disabled = true;
-    // digitBtn.classList.add('disabled');
 }
 
+// Function to generate PIN in range of 1-4 digit
 function generatePin() {
-    const min = 1000;
-    const max = 9999;
-
     //The maximum and the minimum both are inclusive
     const resultPin = Math.floor(Math.random() * (max - min + 1)) + min;
     displayPin.value = resultPin;
 
+    // Submit button enabled when generating PIN
     submitBtn.disabled = false;
     submitBtn.classList.remove('disabled');
-
-    // digitBtn.disabled = false;
-    // digitBtn.classList.remove('disabled');
 }
 
+// Function to add digits into display input on submit section
 function addDigit(event) {
     // If generatePin is not found, assigning digit/value is not allowed
-    if (!displayPin.value) return;
+    if (!displayPin.value) {
+        return alert('Please generate PIN at first!');
+    }
 
-    // When numbers exceeds 4 digit, stop assign value into the display input
-    if (digitCount <= 3) {
-        const digit = event.target.innerText;
+    // When numbers exceeds 4 digits, stop assign value into the display input
+    const digit = event.target.innerText;
+    if (digitCount <= 3 && digit !== '<' && digit !== 'C') {
         displayDigit.value += digit;
         digitCount++;
     }
+}
+
+// Function to delete one digit from left event
+function deleteLeft() {
+
+}
+
+// Function to clear all display digits in the input
+function clearAll() {
+    digitCount = 0;
+    displayDigit.value = '';
 }
 
 function submitPin() {
