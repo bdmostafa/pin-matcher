0b2ff4795ca13f6bafb2f5b791daa80627ffe335
Mostafa <mdmostafaalmahmud@gmail.com>
2020-08-03 11:16:26 +0600

    notifyMessage function process on submitBtn click event

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ let digitCount = 0;
 let tryCount = 3;
 const min = 1000;
 const max = 9999;
+let isSubmitClicked = false;
 
 // Selectors ===========================================================
 const displayPin = document.querySelector('.display-pin');
@@ -12,6 +13,8 @@ const digitBtn = document.querySelectorAll('.button');
 const displayDigit = document.querySelector('.display-digit');
 const successMessage = document.querySelector('.success-message');
 const wrongMessage = document.querySelector('.wrong-message');
+const clearAllBtn = document.querySelector('.clearAll');
+const deleteBtn = document.querySelector('.deleteLeft');
 const tryLeft = document.querySelector('.action-left');
 
 
@@ -20,6 +23,8 @@ const tryLeft = document.querySelector('.action-left');
 document.addEventListener('DOMContentLoaded', submitDisabled);
 generateBtn.addEventListener('click', generatePin);
 digitBtn.forEach((digit) => digit.addEventListener('click', addDigit));
+clearAllBtn.addEventListener('click', clearAll);
+deleteBtn.addEventListener('click', deleteLeft);
 submitBtn.addEventListener('click', submitPin);
 
 
@@ -29,18 +34,25 @@ submitBtn.addEventListener('click', submitPin);
 function submitDisabled() {
     submitBtn.disabled = true;
     submitBtn.classList.add('disabled');
+
+    // Submit button enabled when generating PIN
+    if (generatePin) {
+        submitBtn.disabled = false;
+        submitBtn.classList.remove('disabled');
+    }
 }
 
 // Function to generate PIN in range of 1-4 digit
 function generatePin() {
+    // debugger;
     //The maximum and the minimum both are inclusive
     const resultPin = Math.floor(Math.random() * (max - min + 1)) + min;
     displayPin.value = resultPin;
-    displayDigit.value = '';
+    // displayDigit.value = '';
 
-    // Submit button enabled when generating PIN
-    submitBtn.disabled = false;
-    submitBtn.classList.remove('disabled');
+    submitDisabled();
+    notifyMessage();
+    digitCount = 0;
 }
 
 // Function to add digits into display input on submit section
@@ -52,6 +64,7 @@ function addDigit(event) {
 
     // When numbers exceeds 4 digits, stop assign value into the display input
     const digit = event.target.innerText;
+    console.log(digit)
     if (digitCount <= 3 && digit !== '<' && digit !== 'C') {
         displayDigit.value += digit;
         digitCount++;
@@ -69,23 +82,45 @@ function clearAll() {
     displayDigit.value = '';
 }
 
-function submitPin(event) {
-    if (displayPin.value === displayDigit.value) {
-        successMessage.style.display = 'block';
-        tryLeft.style.display = 'none';
-        submitDisabled();
-    } else {
-        wrongMessage.style.display = 'block';
-        displayDigit.value = '';
-        digitCount = 0;
+function submitPin() {
+    isSubmitClicked = true;
+    // if (displayPin.value === displayDigit.value) {
+    // successMessage.style.display = 'block';
+    notifyMessage();
+    submitDisabled();
+    // } else {
+    // wrongMessage.style.display = 'block';
+    // displayDigit.value = '';
+    // digitCount = 0;
 
-        while (tryCount > 0)
-            tryLeft.innerText = `${tryCount} try left`;
-        tryCount--;
+    // while (tryCount > 0)
+    //     tryLeft.innerText = `${tryCount} try left`;
+    // tryCount--;
 
-        if (tryCount === 0) {
-            submitDisabled();
-            return alert('Oops! Please try again.');
+    // if (tryCount === 0) {
+    //     submitDisabled();
+    //     return alert('Oops! Please try again.');
+    // }
+    // }
+}
+
+function notifyMessage() {
+    successMessage.style.display = 'none';
+    wrongMessage.style.display = 'none'
+
+    // If submitBtn is clicked, process notify message 
+    if (isSubmitClicked) {
+        if (displayPin.value === displayDigit.value) {
+            successMessage.style.display = 'block';
+            displayPin.value = '';
+            displayDigit.value = '';
+            tryLeft.style.display = 'none';
+        } else {
+            wrongMessage.style.display = 'block';
+            displayDigit.value = '';
         }
     }
+    // Finishing notifyMessages again assign 'false' to isSubmitClicked
+    isSubmitClicked = false;
+
 }
