a6027887200bffc5630836d7f237ce46482322cc
Mostafa <mdmostafaalmahmud@gmail.com>
2020-08-04 18:58:25 +0600

    README file created and final rivision

---
+++ README.md
@@ -0,0 +1,17 @@
+# Pin Matcher
+
+    Name: Pin-Matcher
+    Author: Mostafa Al Mahmud
+
+## Tasks/requirements:
+
+    1. Both INPUT display button is disabled to edit
+    2. Before generating PIN, SUBMIT button is disabled to click with low opacity and '3 TRY LEFT' text is hidden
+    3. ALERT to generate PIN when any DIGIT/NUMBER is pressed without getting PIN
+    4. Generate only 4 digits PIN randomly
+    5. ALERT to press DIGIT/NUMBER if SUBMIT is clicked without pressing any numbers
+    6. Able to input MAXIMUM 4 digits
+    7. DELETE left side number when '<' is clicked and CLEAR ALL numbers when 'C' is clicked
+    8. NOTIFY message when SUBMITTING whether it is right or wrong
+    9. 3 TRY LEFT options for only 1 generated PIN if the answers are wrong
+    10. Program reset after 3 TRY or successful SUBMITTING
--- app.js
+++ app.js
@@ -1,21 +1,4 @@
-/*
-Project details:
-    Name: Pin-Matcher
-    Author: Mostafa Al Mahmud
-    Tasks/requirements to be done:
-        1. Both INPUT button is disabled to edit by pointer-events
-        2. Before generating PIN, SUBMIT button is disabled with low opacity and '3 TRY LEFT' text is hidden
-        3. ALERT to generate PIN when any DIGIT/NUMBER is pressed without getting PIN
-        4. Generate only 4 digits PIN randomly
-        5. ALERT to press DIGIT/NUMBER if SUBMIT is clicked without pressing any numbers
-        6. Able to input MAXIMUM 4 digits
-        7. DELETE left side number when '<' is clicked and CLEAR ALL numbers when 'C' is clicked
-        8. NOTIFY message when SUBMITTING whether it is right or wrong
-        9. 3 TRY LEFT options for only 1 generated PIN if the answer is wrong 
-        10. Program reset after 3 TRY or successful SUBMITTING
-*/
-
-// Variables =========================================================
+// Global Variables =========================================================
 let digitCount = 0;
 let tryCount = 3;
 const min = 1000;
@@ -29,7 +12,7 @@ const displayPin = document.querySelector('.display-pin');
 const generateBtn = document.querySelector('.generate-btn');
 const submitBtn = document.querySelector('.submit-btn');
 const digitBtn = document.querySelectorAll('.button');
-const submitDigit = document.querySelector('.submit-digit');
+const displaySubmit = document.querySelector('.display-submit');
 const successMessage = document.querySelector('.success-message');
 const wrongMessage = document.querySelector('.wrong-message');
 const clearAllBtn = document.querySelector('.clearAll');
@@ -66,107 +49,105 @@ function submitDisabled() {
 
 // Function to generate PIN in range of 1-4 digits
 function generatePin() {
-    // debugger;
     //The maximum and the minimum, both are inclusive
     const resultPin = Math.floor(Math.random() * (max - min + 1)) + min;
     displayPin.value = resultPin;
 
-    // When PIN is generated, submitDigit value is empty 
-    submitDigit.value = '';
+    // Whenever a new PIN is generated, displaySubmit value is EMPTY 
+    displaySubmit.value = '';
 
-    // NOTIFY messages display is 'none' from notifyMessage()
+    // NOTIFY messages are hidden when generating PIN
     notifyMessage();
     submitDisabled();
 
-    // Initial value like reloading when generate PIN
+    // When generating PIN, initial value like reloading 
     digitCount = 0;
     tryCount = 3;
     tryLeft.innerText = `${tryCount} try left`;
 }
 
-// Function to add digits into submitDigit on submit section
+// Function to ADD digits into displaySubmit box on submit section
 function addDigit(event) {
-    // When input numbers exceed 4 digits, stop assign value into the display input
+    // When input numbers exceed 4 digits, unable to press further any digit
     const digit = event.target.innerText;
     if (digitCount <= 3 && digit !== '<' && digit !== 'C') {
-        submitDigit.value += digit;
+        displaySubmit.value += digit;
         digitCount++;
 
-        // If generatePin is not found, alert to generate PIN at first
+        // If generatePin is not found, ALERT to generate PIN at first
         alertMessage();
     }
 }
 
-// Function to delete one digit from left event
+// Function to DELETE one digit from left on displaySubmit box
 function deleteLeft() {
-    console.log(submitDigit.value)
-    submitDigit.value = submitDigit.value.slice(0, -1);
+    displaySubmit.value = displaySubmit.value.slice(0, -1);
     digitCount--;
 }
 
-// Function to clear all submitDigit.value in the input box
+// Function to CLEAR ALL displaySubmit.value in the input box
 function clearAll() {
     digitCount = 0;
-    submitDigit.value = '';
+    displaySubmit.value = '';
 }
 
 // Function to submit PIN into the input box
 function submitPin() {
     isSubmitClicked = true;
 
-    // Show result whether it is RIGHT or WRONG
+    // Show result/notify message whether it is RIGHT or WRONG
     notifyMessage();
 
-    // SUBMIT button is active until generate PIN is shown
+    // SUBMIT button is active when generated PIN is shown
     submitDisabled();
 
-    // After every SUBMIT click event, digitCount starting from zero (0)
+    // After every SUBMIT click event, digitCount starting from ZERO (0)
     digitCount = 0;
 }
 
-// Function to NOTIFY the message on right or wrong answer and TRY actions
+// Function to NOTIFY messages and TRY actions
 function notifyMessage() {
-    // NOTIFY messages display is 'none' at first
+    // NOTIFY messages are hidden at first
     successMessage.style.display = 'none';
     wrongMessage.style.display = 'none';
 
     // If submitBtn is clicked, process notify message (success or wrong)
     if (isSubmitClicked) {
-        if (displayPin.value === submitDigit.value) {
+        if (displayPin.value === displaySubmit.value) {
             successMessage.style.display = 'block';
             displayPin.value = '';
-            submitDigit.value = '';
+            displaySubmit.value = '';
             tryLeft.style.display = 'none';
         } else {
             wrongMessage.style.display = 'block';
             --tryCount;
             tryLeft.innerText = `${tryCount} try left`;
 
-            // When 3 times tried, alert message to generate PIN again
+            // Having 3 times tried, ALERT message to generate PIN again
             alertMessage();
-            submitDigit.value = '';
+            displaySubmit.value = '';
         }
     }
-    // Finishing notifyMessages again assign 'false' to isSubmitClicked
+    // Again assign 'false' to isSubmitClicked
     isSubmitClicked = false;
 }
 
-// Function to alert various message on different situations
+// Function to ALERT various messages on different situations
 function alertMessage() {
 
-    // If generatePin is not found, assigning digit/input is not allowed
+    // If generatePin is not found, assigning any digit/input is not allowed
     if (!displayPin.value) {
         alert('Please generate PIN at first!');
         digitCount = 0;
-        return submitDigit.value = '';
+        return displaySubmit.value = '';
     }
 
     // If anyone click SUBMIT button without input a PIN, alert message
-    if (!submitDigit.value) {
+    if (!displaySubmit.value) {
         alert('Please press numbers before submitting!')
     }
 
-    // When 3 times trying, alert this and program reset
+    // Having 3 times tried, ALERT this and program RESET
     if (tryCount <= 0) {
         alert('Oops! You have tried 3 times. Please generate PIN again.');
         displayPin.value = '';
